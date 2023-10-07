d41df5813012def4a0e2b675839c7f0f0dd4804a
travis martin <travismartin266@gmail.com>
2023-10-07 14:12:57 -0300

    update tests add new test to improve coverage on getbyid when no ticket is found

--- parking-service/src/test/java/org/mtravis/microservices/ParkingServiceResourceTest.java
+++ parking-service/src/test/java/org/mtravis/microservices/ParkingServiceResourceTest.java
@@ -3,30 +3,21 @@ package org.mtravis.microservices;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import io.quarkus.test.InjectMock;
-import io.quarkus.test.Mock;
 import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.parsing.Parser;
-import jakarta.enterprise.inject.Any;
-import jakarta.inject.Inject;
 import lombok.SneakyThrows;
-import org.eclipse.jetty.util.ajax.JSON;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.mtravis.microservices.api.endpoint.ParkingServiceResource;
 import org.mtravis.microservices.model.ParkingSpotType;
 import org.mtravis.microservices.model.Ticket;
 import org.mtravis.microservices.model.Vehicle;
-import org.mtravis.microservices.resources.ParkingAllocationInformation;
-import org.mtravis.microservices.resources.SpotAllocationApi;
 
 import java.time.Instant;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.any;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -42,7 +33,7 @@ public class ParkingServiceResourceTest {
     private static final Ticket mockTicket = Ticket.builder()
             .entryTime(Instant.now())
             .id(UUID.randomUUID())
-            .build();//mock(Ticket.class);
+            .build();
     static TicketService mockedTicketService = mock(TicketService.class);
     private final ObjectMapper JSON_MAPPER = new ObjectMapper()
             .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
@@ -64,19 +55,9 @@ public class ParkingServiceResourceTest {
             .body(JSON_MAPPER.writeValueAsString(vehicle))
             .when()
             .post("/api/parking/v2").then()
+                .statusCode(202)
                 .body("ticket_id", is(mockTicket.id.toString()))
                 .defaultParser(Parser.JSON);
-
-
-    }
-    @Test
-    public void testHelloEndpoint() {
-
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
     }
 
 }
--- parking-service/src/test/java/org/mtravis/microservices/ParkingServiceTest.java
+++ parking-service/src/test/java/org/mtravis/microservices/ParkingServiceTest.java
@@ -10,9 +10,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mtravis.microservices.model.Ticket;
+import org.mtravis.microservices.persistence.TicketEntity;
 import org.mtravis.microservices.persistence.TicketRepository;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -21,14 +23,14 @@ import static org.mockito.Mockito.when;
 
 @QuarkusTest
 public class ParkingServiceTest {
-    static TicketRepository mockerRepository = Mockito.mock(TicketRepository.class);
+    static TicketRepository ticketRepository = Mockito.mock(TicketRepository.class);
 
     @Inject
     TicketService service;
 
     @BeforeAll
     static void setup(){
-        QuarkusMock.installMockForType(mockerRepository, TicketRepository.class);
+        QuarkusMock.installMockForType(ticketRepository, TicketRepository.class);
     }
 
     @Test
@@ -51,7 +53,82 @@ public class ParkingServiceTest {
                         .exitTime(Instant.now())
                         .build();
         PanacheQuery panacheQuery = Mockito.mock(PanacheQuery.class);
-        when(mockerRepository.find(anyString(), (Object) any())).thenReturn(panacheQuery);
+        when(ticketRepository.find(anyString(), (Object) any())).thenReturn(panacheQuery);
         Assertions.assertTrue(service.create(ticket));
+    }
+
+    @SneakyThrows
+    @Test
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    void test_create_withConflict(){
+        UUID id = UUID.randomUUID();
+        Ticket ticket =
+                Ticket.builder()
+                        .id(id)
+                        .build();
+        TicketEntity ticketEntity = new TicketEntity();
+        ticketEntity.id = id;
+        PanacheQuery panacheQuery = Mockito.mock(PanacheQuery.class);
+        when(ticketRepository.find(anyString(), (Object) any())).thenReturn(panacheQuery);
+        when(panacheQuery.firstResult()).thenReturn(ticketEntity);
+
+        Assertions.assertFalse(service.create(ticket));
+    }
+
+    @Test
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    void test_delete(){
+        PanacheQuery panacheQuery = Mockito.mock(PanacheQuery.class);
+        when(ticketRepository.find(anyString(), (Object) any())).thenReturn(panacheQuery);
+
+        TicketEntity ticket = new TicketEntity();
+        ticket.id = UUID.randomUUID();
+        when(panacheQuery.firstResult()).thenReturn(ticket);
+
+        Assertions.assertTrue(service.delete(ticket.id));
+    }
+    @Test
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    void test_delete_notExist(){
+        PanacheQuery panacheQuery = Mockito.mock(PanacheQuery.class);
+        when(ticketRepository.find(anyString(), (Object) any())).thenReturn(panacheQuery);
+        when(panacheQuery.firstResult()).thenReturn(null);
+
+        Assertions.assertFalse(service.delete(UUID.randomUUID()));
+    }
+
+    @SneakyThrows
+    @Test
+    void test_getAll(){
+        TicketEntity ticketEntity = new TicketEntity();
+        ticketEntity.id = UUID.randomUUID();
+
+        when(ticketRepository.listAll()).thenReturn(List.of(ticketEntity));
+
+        Assertions.assertEquals(1, service.getAll().size());
+    }
+
+    @SneakyThrows
+    @Test
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    void test_getById(){
+        UUID id = UUID.randomUUID();
+        TicketEntity ticketEntity = new TicketEntity();
+        ticketEntity.id = id;
+
+        PanacheQuery panacheQuery = Mockito.mock(PanacheQuery.class);
+        when(ticketRepository.find(anyString(), (Object) any())).thenReturn(panacheQuery);
+        when(panacheQuery.firstResult()).thenReturn(ticketEntity);
+
+        Assertions.assertEquals(id, service.getById(id).id);
+    }
+
+    @SneakyThrows
+    @Test
+    void test_getById_NotFound(){
+        PanacheQuery panacheQuery = Mockito.mock(PanacheQuery.class);
+        when(ticketRepository.find(anyString(), (Object) any())).thenReturn(panacheQuery);
+        when(panacheQuery.firstResult()).thenReturn(null);
+        Assertions.assertNull(service.getById(UUID.randomUUID()));
     }
 }
